288e9a14bd597fb5bc6b92ce27cafbce6d9644a8
MoeBrowne <moebrowne@users.noreply.github.com>
2016-10-11 17:07:37 +0100

    Fall back to zip download
    When git is unavailable download the zip file of the repo

--- bin/install.sh
+++ bin/install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-[[ -x $(which git) ]] && git submodule update --init --recursive || echo "You don't have git installed, so can't update the list"
+if [[ -x $(which git) ]]; then
+	git submodule update --init --recursive
+elif [[ -x $(which curl) && -x $(which unzip) ]]; then
+	curl -L https://github.com/minimaxir/big-list-of-naughty-strings/archive/master.zip -o BLoNS.zip &&
+	unzip -o BLoNS.zip -d resources &&
+	rm BLoNS.zip
+else
+	echo "Can't update the Big List Of Naughty Strings"
+fi
